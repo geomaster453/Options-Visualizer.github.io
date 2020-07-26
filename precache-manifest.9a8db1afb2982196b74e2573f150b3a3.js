c9dc1e84a9ce8405ccac1cb200bacfb97a98af36
geomaster453 <austin.wei@lakesideschool.org>
2020-07-26 04:27:55 -0700

    Updates

--- precache-manifest.9a8db1afb2982196b74e2573f150b3a3.js
+++ precache-manifest.9a8db1afb2982196b74e2573f150b3a3.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4f50225e9385233a0af6d9cb17e3b28c",
+    "revision": "502042c2587b3e9bd3b3e4f16ab719e3",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "bf1c9f6f9ab7e54e6ff6",
+    "revision": "96f031819c8e598288fa",
     "url": "/static/css/main.d6490fbc.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.47e7d80b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bf1c9f6f9ab7e54e6ff6",
-    "url": "/static/js/main.0460341b.chunk.js"
+    "revision": "96f031819c8e598288fa",
+    "url": "/static/js/main.7865eeb1.chunk.js"
   },
   {
     "revision": "a8f5c78092a82cf6f7de",
